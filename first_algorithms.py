334cb0896924eb1a5205bc500824f276072add7e
justdiyas <iyemberdiyev@gmail.com>
2024-04-20 14:51:49 +0500

    defined a function that locates the position of a target card in a list of cards. Brute force solution (linear algorithm) is implemented

---
+++ first_algorithms.py
@@ -0,0 +1,34 @@
+from random import shuffle
+
+
+#a function that locates a target card in a given set of cards
+def locate_card(cards, target):
+    pass
+
+
+#test cases for different scenarios including edge cases
+tests = []
+
+#running a function for a test case
+# locate_card(**tests[0]['input']) == tests[0]['output'])
+
+#create a list of numbers that represent cards and shuffle it
+cards = [i for i in range(50)]
+shuffle(cards)
+
+
+#solving a problem using brute force solution that executes linear search algorithm,
+#that is to check all answers from 0 until answer is found and return -1 if target is not in the list.
+def locate_card(cards, target):
+    index = 0
+    found = False
+    while not found:
+        if cards[index] == target:
+            found = True
+            return index
+        index += 1
+        if index == len(cards):
+            return -1
+
+
+print(locate_card(cards, 10))
