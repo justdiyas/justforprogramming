0abaf1713754b51931682e431fd78306749e4d49
justdiyas <iyemberdiyev@gmail.com>
2024-06-21 12:27:59 +0500

    Created car class, created new insert and added data into tables

--- employee.py
+++ employee.py
@@ -17,3 +17,14 @@ class Employee:
 
     def __repr__(self):
         return f'First name: {self.first} \nLast name: {self.last} \nProfession: {self.profession} \nPay rate: {self.pay}'
+
+
+class Car:
+    def __init__(self, car_id, make, model):
+        self.car_id = car_id
+        self.make = make
+        self.model = model
+
+    def __repr__(self):
+        return f'Make: {self.make} \nModel: {self.model}'
+
--- first_db.py
+++ first_db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from employee import Employee
+from employee import Employee, Car
 
 connection = sqlite3.connect('employee.db')
 cursor = connection.cursor()
@@ -24,6 +24,11 @@ def insert_emp(emp):
     with connection:
         cursor.execute("INSERT INTO employees VALUES (:id, :first_name, :last_name, :profession, :pay, :car_id);",
                {'id': emp.id, 'first_name': emp.first, 'last_name': emp.last, 'profession': emp.profession, 'pay': emp.pay, 'car_id': emp.car_id})
+
+def insert_car(car):
+    with connection:
+        cursor.execute("INSERT INTO cars VALUES (:car_id, :make, :model);",
+               {'car_id': car.car_id, 'make': car.make, 'model': car.model})
 
 
 def get_emp_by_name(first_name):
@@ -52,14 +57,20 @@ def get_all_from_table(table):
 def drop_table(table):
     cursor.execute(f"DROP TABLE {table}")
 
-emp_5 = Employee(1, 'Abay', 'Kerimov', 'Go Developer', 800000, None)
-emp_6 = Employee(2, 'Gulmira', 'Sarsenova', 'Nurse', 300000, None)
-
-insert_emp(emp_5)
-insert_emp(emp_6)
+# emp_5 = Employee(1, 'Abay', 'Kerimov', 'Go Developer', 800000, None)
+# emp_6 = Employee(2, 'Gulmira', 'Sarsenova', 'Nurse', 300000, None)
+#
+# insert_emp(emp_5)
+# insert_emp(emp_6)
 # create_table()
 # drop_table('employees')
 # drop_table('cars')
+car_1 = Car(1, 'Toyota', 'Camry 10')
+car_2 = Car(2, 'Nissan', 'Premiera')
+car_3 = Car(3, 'Mitsubishi', 'Galant')
+insert_car(car_1)
+insert_car(car_2)
+insert_car(car_3)
 get_all_from_table('employees')
 get_all_from_table('cars')
 
